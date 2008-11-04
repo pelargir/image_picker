bb8164af68787bc2878705feedfeb423ef1a2bcb
Matthew Bass <matt@anacreon.local>
2008-11-03 22:02:36 -0500

    updated asset_copier

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
-require 'asset_copier'
+require 'image_picker/asset_copier'
 
 if RAILS_ENV == "development"
-  AssetCopier.copy "image_picker"
+  ImagePicker::AssetCopier.copy "image_picker"
 else
-  AssetCopier.warn "image_picker"
+  ImagePicker::AssetCopier.warn "image_picker"
 end
